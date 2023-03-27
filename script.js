ef147cb6c2bd0b45d710232f2c6af3f9b14739c2
Evan Azhar <gshsdhudh3@gmail.com>
2023-03-27 17:42:16 +0700

    percobaan 1

---
+++ script.js
@@ -0,0 +1,26 @@
+const menuToggle = document.querySelector('.menu-toggle a');
+const nav = document.querySelector('nav ul');
+const menuToggle2 = document.querySelector('.menu-toggle');
+const menuToggleChange = document.querySelector('.menu-toggle a i');
+
+// menu toggle
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+    menuToggleChange.classList.toggle('change');
+});
+
+
+// // klik diiluar untuk off function
+document.addEventListener('click',function(e){
+    if(!menuToggle.contains(e.target) && !nav.contains(e.target)){
+    nav.classList.remove('slide')
+    menuToggleChange.classList.remove('change');
+    }
+})
+
+document.addEventListener('click',function(e){
+    if(!menuToggle.contains(e.target) && !nav.contains(e.target)){
+    menuToggle2.classList.remove('input')
+    }
+})
+
